a7b43ccd929ec6ec8bd927d303063685b203ce27
eliozashvili <ilivhsazoile.g@gmail.com>
2026-06-17 00:57:21 +0400

    feat: simple chunk spawner with for loop

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [SerializeField] private GameObject chunkPrefab;
+    [SerializeField] private int chunkAmount;
+    [SerializeField] private Transform chunkParent;
+    [SerializeField] private int chunkLength;
+
     private void Start()
     {
-        Debug.Log("Hello, World!");
+        for (var i = 0; i < chunkAmount; i++)
+        {
+            var spawnPositionZ = transform.position.z + chunkLength * i;
+            var chunkPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
+            // Fourth parameter tells Instantiate to generate
+            // chunkPrefabs under chunkParent GameObject
+            Instantiate(chunkPrefab, chunkPos, Quaternion.identity, chunkParent);
+        }
     }
 }
