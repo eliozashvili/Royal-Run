9b64ca86bf517672743748c048efbb78d680c7e9
eliozashvili <ilivhsazoile.g@gmail.com>
2026-06-18 16:21:59 +0400

    feat: floor chunks moving towards player, shadow normal bias set to 0

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -6,8 +6,21 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private int chunkAmount;
     [SerializeField] private Transform chunkParent;
     [SerializeField] private int chunkLength;
+    [SerializeField] private float chunkMoveSpeed;
+
+    private GameObject[] _chunks = new GameObject[12];
 
     private void Start()
+    {
+        SpawnChunks();
+    }
+
+    private void Update()
+    {
+        MoveChunks();
+    }
+
+    private void SpawnChunks()
     {
         for (var i = 0; i < chunkAmount; i++)
         {
@@ -15,7 +28,18 @@ public class LevelGenerator : MonoBehaviour
             var chunkPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
             // Fourth parameter tells Instantiate to generate
             // chunkPrefabs under chunkParent GameObject
-            Instantiate(chunkPrefab, chunkPos, Quaternion.identity, chunkParent);
+            var newChunk = Instantiate(chunkPrefab, chunkPos, Quaternion.identity, chunkParent);
+
+            _chunks[i] = newChunk;
+        }
+    }
+
+    private void MoveChunks()
+    {
+        foreach (var chunk in _chunks)
+        {
+            // Moving every chunk in _chunks array backwards
+            chunk.transform.Translate(Vector3.back * (Time.deltaTime * chunkMoveSpeed));
         }
     }
 }
