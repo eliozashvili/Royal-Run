8323adbf6fec2521f0019d806c92f81dfd890fd8
eliozashvili <ilivhsazoile.g@gmail.com>
2026-06-19 20:36:47 +0400

    feat: clamp movement on x, z axis. minor refactor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float xRange;
+    [SerializeField] private float zRange;
 
     private Rigidbody _rigidbody;
     private Vector2 _movement;
@@ -15,7 +17,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        HandleMovement();
+        MoveAndClampMovement();
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -23,10 +25,16 @@ public class PlayerController : MonoBehaviour
         _movement = context.ReadValue<Vector2>();
     }
 
-    private void HandleMovement()
+    private void MoveAndClampMovement()
     {
-        var playerInput = new Vector3(_movement.x, 0, _movement.y);
+        // Find step of movement
+        var movementStep = new Vector3(_movement.x, 0, _movement.y) * (Time.fixedDeltaTime * moveSpeed);
+        // Apply step to RigidBody position
+        var targetStep = _rigidbody.position + movementStep;
+        // Clamp step so Player won't walk-off screen
+        targetStep.x = Mathf.Clamp(targetStep.x, -xRange, xRange);
+        targetStep.z = Mathf.Clamp(targetStep.z, -zRange, zRange);
 
-        _rigidbody.MovePosition(_rigidbody.position + playerInput * (Time.fixedDeltaTime * moveSpeed));
+        _rigidbody.MovePosition(targetStep);
     }
 }
