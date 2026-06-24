e59357829073b8dde8b3c48ab7af8026c1071277
eliozashvili <ilivhsazoile.g@gmail.com>
2026-06-24 23:03:22 +0400

    feat: player collision animation

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+    [SerializeField] private float collisionCooldown;
+
+    private const string HitTrigger = "Hit";
+
+    private float _cooldownTimer;
+
+    private void Update()
+    {
+        _cooldownTimer += Time.deltaTime;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.gameObject.name);
+        if (_cooldownTimer < collisionCooldown) return;
+
+        animator.SetTrigger(HitTrigger);
+        _cooldownTimer = 0f;
     }
 }
